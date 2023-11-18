afb5f700116a6614d607c2272c6448d0c130b05d
Thiago Schwartz <perereco@gmail.com>
2023-11-18 09:18:50 -0300

    Adicionada configuração para utilizar o banco de produção

--- config/prod.exs
+++ config/prod.exs
@@ -19,3 +19,13 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+# Configure your database
+config :weather_app_2, WeatherApp2.Repo,
+  maintenance_database: "defaultdb",
+  stacktrace: true,
+  show_sensitive_data_on_connection_error: true,
+  ssl: true,
+  pool_size: 10,
+  ssl_opts: [
+    cacertfile: "priv/cert/ca.pem"
+  ]
